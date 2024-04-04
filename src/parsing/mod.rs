691bafb99b1b3822d169f877eadc04d622bd3684
Dalia Rojas Vallejo <daliarojasvallejo@proton.me>
2024-04-04 10:37:00 -0500

    test: Write tests for processing a markdown file

--- src/parsing/mod.rs
+++ src/parsing/mod.rs
@@ -180,4 +180,73 @@ mod tests {
             consectetur et est culpa et culpa duis.</p>\n"
         );
     }
+
+    #[test]
+    fn process_md_file_works_on_a_file_with_a_frontmatter() {
+        let input = include_str!("test.md");
+
+        let result = process_md_file(input);
+        assert!(result.is_ok());
+
+        let (frontmatter, content) =
+            result.expect("Failed to parse markdown file");
+
+        assert_eq!(frontmatter["description"], "This is a test note");
+        assert_eq!(frontmatter["date"], "2024-04-03");
+
+        assert_eq!(
+            content,
+            "<h1>Lorem ipsum dolor sit amet</h1>\n<p>Lorem \
+            <del>ipsum</del> <em>dolor</em> sit amet, officia excepteur ex \
+            fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur \
+            mollit ex esse <strong>exercitation</strong> amet. Nisi anim \
+            cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum \
+            Lorem est aliquip amet voluptate voluptate dolor minim nulla est \
+            proident. Nostrud officia pariatur ut officia. Sit irure elit esse \
+            ea nulla sunt ex occaecat reprehenderit commodo officia dolor \
+            Lorem duis laboris cupidatat officia voluptate. Culpa proident \
+            adipisicing id nulla nisi laboris ex in Lorem sunt duis officia \
+            eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt \
+            velit enim. Voluptate laboris sint cupidatat ullamco ut ea \
+            consectetur et est culpa et culpa duis.</p>\n"
+        )
+    }
+
+    #[test]
+    fn process_md_file_works_on_a_file_with_an_empty_frontmatter() {
+        let input = "---\n---\n# This is a heading\n\nThis is a paragraph";
+
+        let result = process_md_file(input);
+        assert!(result.is_ok());
+
+        let (_, content) = result.expect("Failed to parse markdown input");
+        assert_eq!(
+            content,
+            "<h1>This is a heading</h1>\n<p>This is a paragraph</p>"
+        );
+    }
+
+    #[test]
+    fn process_md_file_errors_on_a_file_with_an_invalid_frontmatter() {
+        let input = include_str!("toml_test.md");
+
+        let result = process_md_file(input);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn process_md_file_errors_with_empty_input() {
+        let input = "";
+
+        let result = process_md_file(input);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn process_md_file_errors_when_frontmatter_is_not_present() {
+        let input = "# This is a heading\n\nThis is a paragraph";
+
+        let result = process_md_file(input);
+        assert!(result.is_err());
+    }
 }
