7390d376fe8d3b9aca4d26b3a6fc13ea49b52a37
Dalia Rojas Vallejo <daliarojasvallejo@proton.me>
2024-04-06 10:25:22 -0500

    test(args): Use proptest to test arbitrary inputs in arg rendering

--- src/execution/arguments.rs
+++ src/execution/arguments.rs
@@ -17,20 +17,53 @@ pub fn render_template_with_args(
 #[cfg(test)]
 mod tests {
     use super::*;
+    use proptest::prelude::*;
+    use tera::escape_html;
 
-    #[test]
-    fn render_template_with_args_works_with_valid_input() {
-        let template = "<body>{{ content }}</body>";
+    proptest! {
+        #[test]
+        fn render_template_works_with_valid_input(input in "\\PC*") {
+            let template = "<body>{{ content }}</body>";
 
-        let result = render_template_with_args(
-            template,
-            Some("This is an example"),
-            false,
-        );
-        assert!(result.is_ok());
+            let result = render_template_with_args(template, Some(&input), false);
+            assert!(result.is_ok());
 
-        let output = result.expect("Failed to render template");
-        assert_eq!(output, "<body>This is an example</body>");
+            let output = result.expect("Failed to render template");
+            assert_eq!(output, format!("<body>{input}</body>"));
+        }
+
+        #[test]
+        fn render_template_with_args_works_with_unnecesary_input(input in "\\PC*") {
+            let template = r#"<body>{{ "Hello, " ~ "world" ~ `!`}}</body>"#;
+
+            let result = render_template_with_args(
+                template,
+                Some(&input),
+                false,
+            );
+            assert!(result.is_ok());
+
+            let output = result.expect("Failed to render template");
+            assert_eq!(output, "<body>Hello, world!</body>");
+        }
+
+        #[test]
+        fn render_template_with_args_correctly_escapes_input(input in "\\PC*") {
+            let template = "<body>{{ content }}</body>";
+
+            let result = render_template_with_args(
+                template,
+                Some(&format!("<h1>{input}</h1>")),
+                true,
+            );
+            assert!(result.is_ok());
+
+            let output = result.expect("Failed to render template");
+            assert_eq!(
+                output,
+                format!("<body>&lt;h1&gt;{}&lt;&#x2F;h1&gt;</body>", escape_html(&input))
+            );
+        }
     }
 
     #[test]
@@ -45,43 +78,10 @@ mod tests {
     }
 
     #[test]
-    fn render_template_with_args_works_with_unnecesary_input() {
-        let template = r#"<body>{{ "Hello, " ~ "world" ~ `!`}}</body>"#;
-
-        let result = render_template_with_args(
-            template,
-            Some("This will be ignored"),
-            false,
-        );
-        assert!(result.is_ok());
-
-        let output = result.expect("Failed to render template");
-        assert_eq!(output, "<body>Hello, world!</body>");
-    }
-
-    #[test]
-    fn render_template_with_args_fails_with_invalid_input() {
+    fn render_template_with_args_fails_without_necessary_input() {
         let template = "<body>{{ content }}</body>";
 
         let result = render_template_with_args(template, None, false);
         assert!(result.is_err());
-    }
-
-    #[test]
-    fn render_template_with_args_correctly_escapes_input() {
-        let template = "<body>{{ content }}</body>";
-
-        let result = render_template_with_args(
-            template,
-            Some("<h1>This is dangerous HTML</h1>"),
-            true,
-        );
-        assert!(result.is_ok());
-
-        let output = result.expect("Failed to render template");
-        assert_eq!(
-            output,
-            "<body>&lt;h1&gt;This is dangerous HTML&lt;&#x2F;h1&gt;</body>"
-        );
     }
 }
